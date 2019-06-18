2f37dc368c5e3bd40325da251a42ec1b1a1011a0
Travis <mitchellkrog@gmail.com>
2019-06-18 13:45:06 +0200

    V1.2019.06.3103 [PyFunceble]

--- .dev-tools/db_types/mariadb.sql
+++ .dev-tools/db_types/mariadb.sql
@@ -1,3 +1,60 @@
+-- -- The tool to check the availability or syntax of domains, IPv4 or URL.
+-- --
+-- ::
+--
+--
+--     ██████╗ ██╗   ██╗███████╗██╗   ██╗███╗   ██╗ ██████╗███████╗██████╗ ██╗     ███████╗
+--     ██╔══██╗╚██╗ ██╔╝██╔════╝██║   ██║████╗  ██║██╔════╝██╔════╝██╔══██╗██║     ██╔════╝
+--     ██████╔╝ ╚████╔╝ █████╗  ██║   ██║██╔██╗ ██║██║     █████╗  ██████╔╝██║     █████╗
+--     ██╔═══╝   ╚██╔╝  ██╔══╝  ██║   ██║██║╚██╗██║██║     ██╔══╝  ██╔══██╗██║     ██╔══╝
+--     ██║        ██║   ██║     ╚██████╔╝██║ ╚████║╚██████╗███████╗██████╔╝███████╗███████╗
+--     ╚═╝        ╚═╝   ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚══════╝╚═════╝ ╚══════╝╚══════╝
+--
+-- This file is part of the PyFunceble project. It provide the MariaDB database structure.
+--
+-- Author:
+--     Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+--
+-- Special thanks:
+--     https://pyfunceble.github.io/special-thanks.html
+--
+-- Contributors:
+--     https://pyfunceble.github.io/contributors.html
+--
+-- Project link:
+--     https://github.com/funilrys/PyFunceble
+--
+-- Project documentation:
+--     https://pyfunceble.readthedocs.io/en/dev/
+--
+-- Project homepage:
+--     https://pyfunceble.github.io/
+--
+-- License:
+-- ::
+--
+--
+--     MIT License
+--
+--     Copyright (c) 2017, 2018, 2019 Nissar Chababy
+--
+--     Permission is hereby granted, free of charge, to any person obtaining a copy
+--     of this software and associated documentation files (the "Software"), to deal
+--     in the Software without restriction, including without limitation the rights
+--     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--     copies of the Software, and to permit persons to whom the Software is
+--     furnished to do so, subject to the following conditions:
+--
+--     The above copyright notice and this permission notice shall be included in all
+--     copies or substantial portions of the Software.
+--
+--     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--     SOFTWARE.
 
 CREATE TABLE IF NOT EXISTS pyfunceble_auto_continue (
     id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
@@ -12,7 +69,7 @@ CREATE TABLE IF NOT EXISTS pyfunceble_auto_continue (
 );
 
 DELIMITER ///
-CREATE TRIGGER updatePyFuncebleAutoContinueDates
+CREATE TRIGGER IF NOT EXISTS updatePyFuncebleAutoContinueDates
     BEFORE UPDATE ON pyfunceble_auto_continue FOR EACH ROW
 BEGIN
     IF NEW.modified <= OLD.modified THEN
@@ -25,6 +82,7 @@ CREATE TABLE IF NOT EXISTS pyfunceble_inactive (
     id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
     file_path LONGTEXT NOT NULL,
     subject LONGTEXT NOT NULL,
+    status VARCHAR(12) NOT NULL,
     digest VARCHAR(64) NOT NULL,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -32,7 +90,7 @@ CREATE TABLE IF NOT EXISTS pyfunceble_inactive (
 );
 
 DELIMITER ///
-CREATE TRIGGER updatePyFuncebleInactiveDates
+CREATE TRIGGER IF NOT EXISTS updatePyFuncebleInactiveDates
     BEFORE UPDATE ON pyfunceble_inactive FOR EACH ROW
 BEGIN
     IF NEW.modified <= OLD.modified THEN
@@ -53,7 +111,7 @@ CREATE TABLE IF NOT EXISTS pyfunceble_mining (
 );
 
 DELIMITER ///
-CREATE TRIGGER updatePyFuncebleMiningDates
+CREATE TRIGGER IF NOT EXISTS updatePyFuncebleMiningDates
     BEFORE UPDATE ON pyfunceble_mining FOR EACH ROW
 BEGIN
     IF NEW.modified <= OLD.modified THEN
@@ -75,7 +133,7 @@ CREATE TABLE IF NOT EXISTS pyfunceble_whois (
 );
 
 DELIMITER ///
-CREATE TRIGGER updatePyFuncebleWhoisDates
+CREATE TRIGGER IF NOT EXISTS updatePyFuncebleWhoisDates
     BEFORE UPDATE ON pyfunceble_whois FOR EACH ROW
 BEGIN
     IF NEW.modified <= OLD.modified THEN
--- .dev-tools/db_types/sqlite.sql
+++ .dev-tools/db_types/sqlite.sql
@@ -1,3 +1,60 @@
+-- -- The tool to check the availability or syntax of domains, IPv4 or URL.
+-- --
+-- ::
+--
+--
+--     ██████╗ ██╗   ██╗███████╗██╗   ██╗███╗   ██╗ ██████╗███████╗██████╗ ██╗     ███████╗
+--     ██╔══██╗╚██╗ ██╔╝██╔════╝██║   ██║████╗  ██║██╔════╝██╔════╝██╔══██╗██║     ██╔════╝
+--     ██████╔╝ ╚████╔╝ █████╗  ██║   ██║██╔██╗ ██║██║     █████╗  ██████╔╝██║     █████╗
+--     ██╔═══╝   ╚██╔╝  ██╔══╝  ██║   ██║██║╚██╗██║██║     ██╔══╝  ██╔══██╗██║     ██╔══╝
+--     ██║        ██║   ██║     ╚██████╔╝██║ ╚████║╚██████╗███████╗██████╔╝███████╗███████╗
+--     ╚═╝        ╚═╝   ╚═╝      ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝╚══════╝╚═════╝ ╚══════╝╚══════╝
+--
+-- This file is part of the PyFunceble project. It provide the SQLite database structure.
+--
+-- Author:
+--     Nissar Chababy, @funilrys, contactTATAfunilrysTODTODcom
+--
+-- Special thanks:
+--     https://pyfunceble.github.io/special-thanks.html
+--
+-- Contributors:
+--     https://pyfunceble.github.io/contributors.html
+--
+-- Project link:
+--     https://github.com/funilrys/PyFunceble
+--
+-- Project documentation:
+--     https://pyfunceble.readthedocs.io/en/dev/
+--
+-- Project homepage:
+--     https://pyfunceble.github.io/
+--
+-- License:
+-- ::
+--
+--
+--     MIT License
+--
+--     Copyright (c) 2017, 2018, 2019 Nissar Chababy
+--
+--     Permission is hereby granted, free of charge, to any person obtaining a copy
+--     of this software and associated documentation files (the "Software"), to deal
+--     in the Software without restriction, including without limitation the rights
+--     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+--     copies of the Software, and to permit persons to whom the Software is
+--     furnished to do so, subject to the following conditions:
+--
+--     The above copyright notice and this permission notice shall be included in all
+--     copies or substantial portions of the Software.
+--
+--     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+--     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+--     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+--     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+--     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+--     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+--     SOFTWARE.
 
 CREATE TABLE IF NOT EXISTS auto_continue (
     id INTEGER PRIMARY KEY,
@@ -10,7 +67,7 @@ CREATE TABLE IF NOT EXISTS auto_continue (
     UNIQUE(file_path, subject)
 );
 
-CREATE TRIGGER updateAutoContinueDates
+CREATE TRIGGER IF NOT EXISTS updateAutoContinueDates
     AFTER UPDATE
     ON auto_continue
     FOR EACH ROW
@@ -23,12 +80,13 @@ CREATE TABLE IF NOT EXISTS inactive (
     id INTEGER PRIMARY KEY,
     file_path TEXT NOT NULL,
     subject TEXT NOT NULL,
+    status INTEGER NOT NULL,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     modified TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     UNIQUE(file_path, subject)
 );
 
-CREATE TRIGGER updateInactiveDates
+CREATE TRIGGER IF NOT EXISTS updateInactiveDates
     AFTER UPDATE
     ON inactive
     FOR EACH ROW
@@ -47,7 +105,7 @@ CREATE TABLE IF NOT EXISTS mining (
     UNIQUE(file_path, subject, mined)
 );
 
-CREATE TRIGGER updateMiningDates
+CREATE TRIGGER IF NOT EXISTS updateMiningDates
     AFTER UPDATE
     ON mining
     FOR EACH ROW
@@ -67,7 +125,7 @@ CREATE TABLE IF NOT EXISTS whois (
     UNIQUE(subject)
 );
 
-CREATE TRIGGER updateWhoisDates
+CREATE TRIGGER IF NOT EXISTS updateWhoisDates
     AFTER UPDATE
     ON whois
     FOR EACH ROW
